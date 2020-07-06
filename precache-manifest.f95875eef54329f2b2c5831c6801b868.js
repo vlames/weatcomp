3e545993c0a712069e64152fd96c6d0037471baa
Vladimir Meshcheryakov <vlames@pdx.edu>
2020-07-06 14:27:06 -0700

    Updates

---
+++ precache-manifest.f95875eef54329f2b2c5831c6801b868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2edd5a542ad204521ee5eb53cc0c9a6",
+    "url": "/weatcomp/index.html"
+  },
+  {
+    "revision": "79ba3e6a0b69f1d81971",
+    "url": "/weatcomp/static/css/main.4e71af12.chunk.css"
+  },
+  {
+    "revision": "7d6be5cbbe6519755946",
+    "url": "/weatcomp/static/js/2.f8742248.chunk.js"
+  },
+  {
+    "revision": "50a84039fafb0cdae22acf252da5fca4",
+    "url": "/weatcomp/static/js/2.f8742248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ba3e6a0b69f1d81971",
+    "url": "/weatcomp/static/js/main.b49fac71.chunk.js"
+  },
+  {
+    "revision": "5aff8a62d09573072d9a",
+    "url": "/weatcomp/static/js/runtime-main.2580aa34.js"
+  }
+]);
